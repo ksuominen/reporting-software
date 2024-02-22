ad6c78a0bf1b499e4eb3806a6fd9e0ca6667b1ff
aleksikantonen <aleksi.kantonen@skillio.ai>
2024-02-22 10:53:36 +0200

    date added to cumulative hours

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from src.queries import db_get_workhours
+from src.queries import db_get_workhours, db_cumulative_hours_by_customers
 from src.write_and_store import write_to_file, send_blob_to_azure
 from datetime import datetime
 
@@ -20,7 +20,8 @@ def send_report_by_day(date):
         return {"error": "incorrect date format,  should be YYYYMMDD"}
     try:
         workhours = db_get_workhours(date_ob)
-        write_to_file(workhours, date_ob)
+        cumulative_hours = db_cumulative_hours_by_customers(date_ob)
+        write_to_file(workhours, cumulative_hours, date_ob)
         send_blob_to_azure(date_ob)
         return {"success": "report sent to Azure"}
     except:
